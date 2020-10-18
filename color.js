a41b47b685825ec7333eb871da12b700567c0a76
agarwalminks <61506798+agarwalminks@users.noreply.github.com>
2020-10-18 17:58:37 +0530

    all colors change bg

--- color.js
+++ color.js
@@ -3,8 +3,28 @@ var colors=["rgb(255, 0, 0)","rgb(0, 255, 0)","rgb(0, 0, 255)","rgb(255, 255, 0)
 var squares= document.querySelectorAll(".square");
 var pickedcolor= colors[3];
 var colorfoundout= document.getElementById("colortobefound");
+var msg =document.getElementById("message");
 colorfoundout.textContent = pickedcolor;
 for(var i=0;i<squares.length;i++)
 {
 squares[i].style.background=colors[i];
+squares[i].addEventListener("click",function()
+{
+    var clickedcolor=this.style.background;
+    if(clickedcolor === pickedcolor)
+    {
+    msg.textContent="correct bruh";
+    changecolors(clickedcolor);
+    }
+    else
+    {
+        this.style.background = "#0000";
+        msg.textContent ="try again bruh";
+    }
+})
+}
+function changecolors(color)
+{
+    for(var i=0;i<squares.length;i++)
+    squares[i].style.background = color;
 }
