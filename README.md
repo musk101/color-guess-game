442adcea97fd7759df9d386fde72020a0fecc6c6
agarwalminks <61506798+agarwalminks@users.noreply.github.com>
2020-10-19 00:22:25 +0530

    styling added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # color-guess-game
+This is a very simple guessing game made with the help of JavaScript.
--- color.js
+++ color.js
@@ -1,8 +1,39 @@
-var colors=generaterandomcolors(6)
+var colors=generaterandomcolors(6);
 var squares= document.querySelectorAll(".square");
 var pickedcolor= pickcolor();
+var h1=document.querySelector("h1");
 var colorfoundout= document.getElementById("colortobefound");
 var msg =document.getElementById("message");
+var easybtn=document.getElementById("e");
+easybtn.addEventListener("click",function(){
+    hardbtn.classList.remove("selected");
+    easybtn.classList.add("selected");
+    colors= generaterandomcolors(3);
+    pickedcolor= pickedcolor();
+    colorfoundout.textContent= pickedcolor;
+    for(var i=0;i<squares.length;i++)
+    {
+    if(colors[i])
+    {
+        squares[i].style.background = colors[i];
+    }
+    else
+    {
+        squares[i].style.display= "none";
+    }}});
+var hardbtn=document.getElementById("h");
+hardbtn.addEventListener("click",function(){
+    easybtn.classList.remove("selected");
+    hardbtn.classList.add("selected");
+})
+var resetbutton =document.querySelector("#reset");
+resetbutton.addEventListener("click",function()
+{
+   colors= generaterandomcolors(6)
+   pickedcolor =pickcolor();
+   colorfoundout.textContent=pickedcolor;
+
+})
 colorfoundout.textContent = pickedcolor;
 for(var i=0;i<squares.length;i++)
 {
@@ -14,6 +45,8 @@ squares[i].addEventListener("click",function()
     {
     msg.textContent="correct bruh";
     changecolors(clickedcolor);
+    h1.style.background= pickedcolor;
+    resetbutton.textContent="play again?";
     }
     else
     {
